fa50f721b02d6948d622207251c7e7dda71cd7c2
jxtorrez <jtorrez@pragmainvest.com.bo>
2023-10-20 14:45:32 -0400

    ultimos arreglos

--- src/app/tesoreria/entidadestipoentidad/servicios/entidades.service.ts
+++ src/app/tesoreria/entidadestipoentidad/servicios/entidades.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { ConsumoApiService } from 'src/app/core/services/consumoApi.service';
 import { environment } from 'src/environments/environment';
 
 
@@ -8,42 +9,42 @@ import { environment } from 'src/environments/environment';
   providedIn: 'root'
 })
 export class EntidadesService {
-  apiUrl = environment.apiUrl ;
+  apiUrl = '' ;
   apiName = 'entidades';
   prefix = '';
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,private apiService : ConsumoApiService) { }
 
   setPrefix(prefix: string){
     this.prefix = prefix;
   }
 
   register(datos: any) {
-    return this.http.post(this.apiUrl + this.prefix+ `/${this.apiName}`, datos);
+    return this.apiService.tesoreria.post(this.apiUrl + this.prefix+ `/${this.apiName}`, datos);
   }
 
   update(datos: any, id: any): Observable<any> {
-    return this.http.put(this.apiUrl + this.prefix+ `/${this.apiName}/${id}`, datos);
+    return this.apiService.tesoreria.put(this.apiUrl + this.prefix+ `/${this.apiName}/${id}`, datos);
   }
 
   find(id:string = '') {
-    return this.http.get(this.apiUrl + this.prefix+ `/${this.apiName}/${id}`);
+    return this.apiService.tesoreria.get(this.apiUrl + this.prefix+ `/${this.apiName}/${id}`);
   }
 
   getAll(size: number = 100, page: number = 1, sortBy:string = 'id', descending:false, keyword:any = '') {
-    return this.http.get(this.apiUrl + this.prefix+ `/${this.apiName}?size=${size}&page=${page-1}&sortBy=${sortBy}&descending=${descending}&keyword=${keyword}`);
+    return this.apiService.tesoreria.get(this.apiUrl + this.prefix+ `/${this.apiName}?size=${size}&page=${page-1}&sortBy=${sortBy}&descending=${descending}&keyword=${keyword}`);
   }
 
   delete(id: string | number): Observable<any> {
-    return this.http.delete(this.apiUrl + this.prefix+ `/${this.apiName}/${id}`);
+    return this.apiService.tesoreria.delete(this.apiUrl + this.prefix+ `/${this.apiName}/${id}`);
   }
 
   habilitar(datos:any,id: string | number): Observable<any> {
     datos['Tipos de Entidad'] = 'habilitar';
-    return this.http.put(this.apiUrl + this.prefix+ `/${this.apiName}/${datos.id}`, datos);
+    return this.apiService.tesoreria.put(this.apiUrl + this.prefix+ `/${this.apiName}/${datos.id}`, datos);
   }
 
   deshabilitar(datos:any,id: string | number): Observable<any> {
     datos['Tipos de Entidad'] = 'deshabilitar';
-    return this.http.put(this.apiUrl + this.prefix+ `/${this.apiName}/${datos.id}`, datos);
+    return this.apiService.tesoreria.put(this.apiUrl + this.prefix+ `/${this.apiName}/${datos.id}`, datos);
   }
 }
