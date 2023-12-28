28c517cc435a26b3f76c5d4a53839d0664a267c9
guxtrax <dark_gusy@hotmail.com>
2023-12-28 13:23:33 -0400

    Ajuste Anticipo compenzacion Front

--- src/app/tesorery/anticipo/aplicacion/formulario/formulario.component.ts
+++ src/app/tesorery/anticipo/aplicacion/formulario/formulario.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { BsLocaleService } from "ngx-bootstrap/datepicker";
 import { NotificacionService } from 'src/app/core/services/notificacion.service';
@@ -14,14 +14,15 @@ import { EstadoAnticipoService } from 'src/app/tesorery/services/tesoreria/estad
   templateUrl: './formulario.component.html',
   styleUrls: ['./formulario.component.scss']
 })
-export class FormularioComponent implements OnInit{
+export class FormularioComponent implements OnInit {
   breadCrumbItems: Array<{}>;
   breadCrumbTitle: string = "Gestión de Anticipos";
   titulo: any = "Movimiento";
-  
+
   routeApi = 'anticipo';
   levelNavigate = 2;
   service = null;
+  inout: boolean = false;
 
   @Input() maxDate: any;
 
@@ -47,29 +48,29 @@ export class FormularioComponent implements OnInit{
     private route: ActivatedRoute,
     private FormBuilder: FormBuilder,
     private notificacionService: NotificacionService,
-    private centroCostoService:CentrocostoService,
-    private entidadService:EntidadService,
-    private aplicacionAnticipoService:AplicacionAnticipoService,
-    private estadoAnticipoService:EstadoAnticipoService,
+    private centroCostoService: CentrocostoService,
+    private entidadService: EntidadService,
+    private aplicacionAnticipoService: AplicacionAnticipoService,
+    private estadoAnticipoService: EstadoAnticipoService,
     private _localeService: BsLocaleService,
-  ){
+  ) {
     this._localeService.use('es');
   }
 
   ngOnInit(): void {
 
     this.maxDate = this.dateNow;
-  
-    this.breadCrumbItems = [ { label: this.breadCrumbTitle },{ label: this.titulo, active: true },];
+
+    this.breadCrumbItems = [{ label: this.breadCrumbTitle }, { label: this.titulo, active: true },];
     this.formGroup = this.FormBuilder.group(this.fieldsFormValidation());
-    if(this.idRuta) this.form['id'].disable();
+    if (this.idRuta) this.form['id'].disable();
     this.getEstadoAnticipo();
     this.getCentroCostos();
     if (this.anticipo) {
-  
+
       this.formGroup.setValue({
         id: this.anticipo.id,
-       // entidadReferencialId: this.anticipo.entidadReferencialId,
+        // entidadReferencialId: this.anticipo.entidadReferencialId,
         fecha: new Date(this.anticipo.fecha),
         monto: this.anticipo.monto,
         estado: this.anticipo.estado,
@@ -80,67 +81,80 @@ export class FormularioComponent implements OnInit{
     }
   }
 
-  
+
   get form() {
     return this.formGroup.controls;
   }
 
   guardar() {
-   
+
     let data = this.formGroup.value;
     data.movimiento = "MOV-PROV";
     data.anticipoId = this.route.snapshot.paramMap.get('id');
-    console.log('<----->',data);
+
+
     this.submitted = true;
     if (this.formGroup.valid) {
       if (this.anticipo) {
-        data.saldo = this.anticipo.monto;        
+        data.saldo = this.anticipo.monto;
         this.aplicacionAnticipoService.update(data).subscribe((res: any) => {
-        
+
           this.notificacionService.successStandar();
           this.alActualizar.emit(res);
-        },(err: any) => {
+        }, (err: any) => {
           this.notificacionService.alertError(err);
         });
-      
+
       } else {
         data.saldo = data.monto;
         console.log(data);
         this.aplicacionAnticipoService.register(data).subscribe((res: any) => {
           this.notificacionService.successStandar();
           this.alGuardar.emit(res);
-        },(err: any) => {
+        }, (err: any) => {
           this.notificacionService.alertError(err);
         }
-      );
+        );
       }
     }
   }
 
-  getEstadoAnticipo(){
-    this.estadoAnticipoService.habilitados().subscribe( data => {
+  getEstadoAnticipo() {
+    this.estadoAnticipoService.habilitados().subscribe(data => {
       this.listaEstadoAnticipo = data.content;
     });
   }
 
-  getCentroCostos(){
+  getCentroCostos() {
     this.centroCostoService.habilitados().subscribe(data => {
       this.listaCentroCostos = data.content;
     });
   }
-  getEntidadReferencia(){
+  getEntidadReferencia() {
     this.entidadService.listaEntidadReferncial().subscribe(data => {
       this.listaEntidades = data.content;
     });
   }
-  
+
   fieldsFormValidation() {
     return {
-      id: ["", []],     
+      id: ["", []],
       monto: [, [Validators.required]],
-      fecha: [, [Validators.required]],    
-      nroReferencia: [, [Validators.required]], 
+      fecha: [, [Validators.required]],
+      nroReferencia: [, [Validators.required]],
       estado: [, [Validators.required]],
+      ingresoEgreso: [, []],
     };
   }
+
+  cambioEstado(event: any) {
+    if (event.nombre === 'COMPENZACION') {
+      this.inout = true;
+      this.formGroup.addControl('ingresoEgreso', new FormControl(null, Validators.required));
+    } else {
+      this.formGroup.removeControl('ingresoEgreso');
+      this.inout = false;
+    }
+  }
 }
+
