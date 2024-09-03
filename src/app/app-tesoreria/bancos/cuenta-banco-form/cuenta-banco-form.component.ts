4981185e41b098366160b364b19e21a542bdc743
Arnaldo Muñoz <kings31lendy@gmail.com>
2024-09-03 15:40:33 -0400

    KERTESO-431 Se añadió el loader en los botones de registro y edición

--- src/app/app-tesoreria/bancos/cuenta-banco-form/cuenta-banco-form.component.ts
+++ src/app/app-tesoreria/bancos/cuenta-banco-form/cuenta-banco-form.component.ts
@@ -1,19 +1,26 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
-import { NotificacionService } from 'src/app/core/services/notificacion.service';
-import { ResponseHandlerService } from 'src/app/core/services/response-handler.service';
-import { ScreenshotService } from 'src/app/core/services/screenshot.service';
-import { BancoService } from 'src/app/core/services/tesoreria/banco.service';
-import { CuentaBancoService } from 'src/app/core/services/tesoreria/cuenta-banco.service';
-import { MonedaService } from 'src/app/core/services/tesoreria/moneda.service';
-import { ApiResponseStandard, ErrorResponseStandard } from 'src/app/shared/interface/common-api-response';
-import { ResponseDataStandard } from 'src/app/shared/interface/common-list-interface';
-import { UtilityService } from 'src/app/shared/services/utilityService.service';
+import { Component, EventEmitter, Input, Output } from "@angular/core";
+import {
+  UntypedFormBuilder,
+  UntypedFormGroup,
+  Validators,
+} from "@angular/forms";
+import { NotificacionService } from "src/app/core/services/notificacion.service";
+import { ResponseHandlerService } from "src/app/core/services/response-handler.service";
+import { ScreenshotService } from "src/app/core/services/screenshot.service";
+import { BancoService } from "src/app/core/services/tesoreria/banco.service";
+import { CuentaBancoService } from "src/app/core/services/tesoreria/cuenta-banco.service";
+import { MonedaService } from "src/app/core/services/tesoreria/moneda.service";
+import {
+  ApiResponseStandard,
+  ErrorResponseStandard,
+} from "src/app/shared/interface/common-api-response";
+import { ResponseDataStandard } from "src/app/shared/interface/common-list-interface";
+import { UtilityService } from "src/app/shared/services/utilityService.service";
 
 @Component({
-  selector: 'cuenta-banco-form',
-  templateUrl: './cuenta-banco-form.component.html',
-  styleUrls: ['./cuenta-banco-form.component.scss']
+  selector: "cuenta-banco-form",
+  templateUrl: "./cuenta-banco-form.component.html",
+  styleUrls: ["./cuenta-banco-form.component.scss"],
 })
 export class CuentaBancoFormComponent {
   @Input() idBanco: string;
@@ -21,111 +28,149 @@ export class CuentaBancoFormComponent {
   @Input() datosBanco: any;
   @Output() cerrarModal = new EventEmitter<void>();
   @Output() alActualizar = new EventEmitter<void>();
-
+  isStatusSubmit: boolean = false;
   formCuentaBanco: UntypedFormGroup;
-  public submitted: boolean = false
+  public submitted: boolean = false;
   public onSubmitFormStatus: boolean = false;
   public listaMonedas: ResponseDataStandard[] = [];
-  labelTipoApertura: string = 'Apertura de cuenta de banco con saldo inicial';
+  labelTipoApertura: string = "Apertura de cuenta de banco con saldo inicial";
   valueInicializacion: boolean = true;
-  totalTransferencia=0;
+  totalTransferencia = 0;
   fechaActual2;
   fechaActual;
 
-  public transferMediumList: ResponseDataStandard[] = []
+  public transferMediumList: ResponseDataStandard[] = [];
 
-	constructor(
-		public BancoService: BancoService,
+  constructor(
+    public BancoService: BancoService,
     private monedaService: MonedaService,
-		private notificacionService: NotificacionService,
+    private notificacionService: NotificacionService,
     private formBuilder: UntypedFormBuilder,
-		private cuentaBancoService: CuentaBancoService,
-		protected utilityService: UtilityService,
-		protected screenshotService: ScreenshotService,
-    private responseHandlerService: ResponseHandlerService,
-	) {}
+    private cuentaBancoService: CuentaBancoService,
+    protected utilityService: UtilityService,
+    protected screenshotService: ScreenshotService,
+    private responseHandlerService: ResponseHandlerService
+  ) {}
 
-  ngOnInit(){
+  ngOnInit() {
     this.getMonedas();
     this.setForm();
   }
 
-  get form() {return this.formCuentaBanco.controls}
+  get form() {
+    return this.formCuentaBanco.controls;
+  }
 
-  setForm(){
+  setForm() {
     let fechaActual = new Date();
-    this.fechaActual = fechaActual.getFullYear() + '-' + (fechaActual.getMonth() + 1).toString().padStart(2, '0') + '-' + fechaActual.getDate().toString().padStart(2, '0');
-    this.fechaActual2 = fechaActual.getFullYear() + '-' + (fechaActual.getMonth() + 1).toString().padStart(2, '0') + '-' + fechaActual.getDate().toString().padStart(2, '0') +'T'+fechaActual.getHours().toString().padStart(2, '0') + ":" + fechaActual.getMinutes().toString().padStart(2, '0')+":"+fechaActual.getSeconds().toString().padStart(2, '0');
-    this.formCuentaBanco = this.formBuilder. group({
-			id: '',
-			nroCuenta: ['',	[
+    this.fechaActual =
+      fechaActual.getFullYear() +
+      "-" +
+      (fechaActual.getMonth() + 1).toString().padStart(2, "0") +
+      "-" +
+      fechaActual.getDate().toString().padStart(2, "0");
+    this.fechaActual2 =
+      fechaActual.getFullYear() +
+      "-" +
+      (fechaActual.getMonth() + 1).toString().padStart(2, "0") +
+      "-" +
+      fechaActual.getDate().toString().padStart(2, "0") +
+      "T" +
+      fechaActual.getHours().toString().padStart(2, "0") +
+      ":" +
+      fechaActual.getMinutes().toString().padStart(2, "0") +
+      ":" +
+      fechaActual.getSeconds().toString().padStart(2, "0");
+    this.formCuentaBanco = this.formBuilder.group({
+      id: "",
+      nroCuenta: [
+        "",
+        [
           Validators.required,
-					Validators.minLength(2),
-					Validators.maxLength(50),
-				],
-			],
-			bancoId: [this.datosBanco['id'], [Validators.required]],
-			monedaId: [null, [Validators.required]],
+          Validators.minLength(2),
+          Validators.maxLength(50),
+        ],
+      ],
+      bancoId: [this.datosBanco["id"], [Validators.required]],
+      monedaId: [null, [Validators.required]],
       inicializacion: [true],
-      montoCuenta: ['', [Validators.required]],
+      montoCuenta: ["", [Validators.required]],
       fechaCuenta: [this.fechaActual2, [Validators.required]],
-      descripcion:['', [
-        Validators.required,
-        Validators.minLength(8),
-        Validators.maxLength(255)
-      ]],
+      descripcion: [
+        "",
+        [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.maxLength(255),
+        ],
+      ],
       //transacciones: this.formBuilder.array([]),
-		})
+    });
   }
 
   getMonedas = (): void => {
     this.monedaService.habilitados().subscribe({
       next: (response: ApiResponseStandard) => {
         this.listaMonedas =
-          this.responseHandlerService?.handleResponseAsArray(response)
+          this.responseHandlerService?.handleResponseAsArray(response);
       },
       error: (error: ErrorResponseStandard) => {
-        this.notificacionService.alertError(error)
+        this.notificacionService.alertError(error);
       },
-    })
-  }
+    });
+  };
 
   onChangeSwitchTransferType = (event): void => {
     this.valueInicializacion = event;
-    if(event){
+    if (event) {
       this.labelTipoApertura = "Apertura de cuenta de banco con saldo inicial";
-    }else{
-      this.labelTipoApertura = "Apertura de cuenta de banco con Transferencia de cuenta(s) existente(s)";
-      this.formCuentaBanco['addControl']('transacciones', this.formBuilder.array([]));
+    } else {
+      this.labelTipoApertura =
+        "Apertura de cuenta de banco con Transferencia de cuenta(s) existente(s)";
+      this.formCuentaBanco["addControl"](
+        "transacciones",
+        this.formBuilder.array([])
+      );
     }
-	}
+  };
 
-  recibirMontoTotal(monto){
+  recibirMontoTotal(monto) {
     this.totalTransferencia = monto;
-    this.formCuentaBanco.get('montoCuenta').setValue(monto);
-   }
+    this.formCuentaBanco.get("montoCuenta").setValue(monto);
+  }
 
   confirmAndContinueSaving = async (): Promise<void> => {
-		this.submitted = true;
-		if (!this.formCuentaBanco.valid) {
-			return
-		}
-		const dataImg = await this.screenshotService?.takeScreenshot('accountFormModalBodyDiv');
-		this.notificacionService?.confirmAndContinueAlert(dataImg, response =>{
-			if(response) this.guardarForm();
+    this.submitted = true;
+    this.isStatusSubmit = true;
+    if (!this.formCuentaBanco.valid) {
+      this.isStatusSubmit = false;
+      return;
     }
-		)
-	}
+    const dataImg = await this.screenshotService?.takeScreenshot(
+      "accountFormModalBodyDiv"
+    );
+    this.notificacionService?.confirmAndContinueAlert(dataImg, (response) => {
+      if (response) this.guardarForm();
+      this.isStatusSubmit = false;
+    });
+  };
 
-  guardarForm(){
+  guardarForm() {
     console.log(this.formCuentaBanco.value);
-    if(this.formCuentaBanco.valid){
-      this.formCuentaBanco.value['movimientosCuentaBanco']= this.formCuentaBanco.value['transacciones'] ? this.formCuentaBanco.value['transacciones'] :null ;
+    if (this.formCuentaBanco.valid) {
+      this.formCuentaBanco.value["movimientosCuentaBanco"] = this
+        .formCuentaBanco.value["transacciones"]
+        ? this.formCuentaBanco.value["transacciones"]
+        : null;
       //console.log(this.formAccionCaja.value)
-      this.cuentaBancoService.register(this.formCuentaBanco.value).subscribe(data=>{
-        this.alActualizar.emit(data);
-        this.notificacionService.successStandar();
-      }, error=>this.notificacionService.alertError(error));
+      this.cuentaBancoService.register(this.formCuentaBanco.value).subscribe(
+        (data) => {
+          this.alActualizar.emit(data);
+          this.notificacionService.successStandar();
+          this.isStatusSubmit = false;
+        },
+        (error) => this.notificacionService.alertError(error)
+      );
     }
     this.submitted = true;
   }
--- src/app/app-tesoreria/cajas/apertura-caja/apertura-caja.component.ts
+++ src/app/app-tesoreria/cajas/apertura-caja/apertura-caja.component.ts
@@ -1,89 +1,128 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
-import { BsModalRef } from 'ngx-bootstrap/modal';
-import { NotificacionService } from 'src/app/core/services/notificacion.service';
-import { ScreenshotService } from 'src/app/core/services/screenshot.service';
-import { MovimientoCajaService } from 'src/app/core/services/tesoreria/movimiento-caja.service';
-import { UtilityService } from 'src/app/shared/services/utilityService.service';
+import { Component, EventEmitter, Input, Output } from "@angular/core";
+import {
+  UntypedFormBuilder,
+  UntypedFormGroup,
+  Validators,
+} from "@angular/forms";
+import { BsModalRef } from "ngx-bootstrap/modal";
+import { NotificacionService } from "src/app/core/services/notificacion.service";
+import { ScreenshotService } from "src/app/core/services/screenshot.service";
+import { MovimientoCajaService } from "src/app/core/services/tesoreria/movimiento-caja.service";
+import { UtilityService } from "src/app/shared/services/utilityService.service";
 
 @Component({
-  selector: 'apertura-caja',
-  templateUrl: './apertura-caja.component.html',
-  styleUrls: ['./apertura-caja.component.scss']
+  selector: "apertura-caja",
+  templateUrl: "./apertura-caja.component.html",
+  styleUrls: ["./apertura-caja.component.scss"],
 })
 export class AperturaCajaComponent {
-
   @Input() datosCaja: any;
-	@Output() alActualizar = new EventEmitter<void>();
+  @Output() alActualizar = new EventEmitter<void>();
   @Output() cerrarModal = new EventEmitter<void>();
   labelOperacion: string = "";
   modalRef?: BsModalRef;
   formAccionCaja: UntypedFormGroup;
-  submitted: boolean=false;
-  totalApertura: number=0;
+  submitted: boolean = false;
+  totalApertura: number = 0;
   fechaActual2;
   fechaActual;
+  isStatusSubmit: boolean = false;
   protected onSubmitFormStatus: boolean = false;
 
   constructor(
-		private movimientoCajaService: MovimientoCajaService,
-		private formBuilder: UntypedFormBuilder,
-		private notificacionService: NotificacionService,
+    private movimientoCajaService: MovimientoCajaService,
+    private formBuilder: UntypedFormBuilder,
+    private notificacionService: NotificacionService,
     private screenshotService: ScreenshotService,
-    protected utilityService: UtilityService,
-	) {}
+    protected utilityService: UtilityService
+  ) {}
 
-  ngOnInit(){
-    if(this.datosCaja){
-      this.labelOperacion = !this.datosCaja['aperturado'] ? "Apertura de Caja" : "Cierre de Caja";
+  ngOnInit() {
+    if (this.datosCaja) {
+      this.labelOperacion = !this.datosCaja["aperturado"]
+        ? "Apertura de Caja"
+        : "Cierre de Caja";
     }
 
     this.setForm();
   }
 
-  setForm(){
+  setForm() {
     let fechaActual = new Date();
-    this.fechaActual = fechaActual.getFullYear() + '-' + (fechaActual.getMonth() + 1).toString().padStart(2, '0') + '-' + fechaActual.getDate().toString().padStart(2, '0');
-    this.fechaActual2 = fechaActual.getFullYear() + '-' + (fechaActual.getMonth() + 1).toString().padStart(2, '0') + '-' + fechaActual.getDate().toString().padStart(2, '0') +'T'+fechaActual.getHours().toString().padStart(2, '0') + ":" + fechaActual.getMinutes().toString().padStart(2, '0')+":"+fechaActual.getSeconds().toString().padStart(2, '0');
+    this.fechaActual =
+      fechaActual.getFullYear() +
+      "-" +
+      (fechaActual.getMonth() + 1).toString().padStart(2, "0") +
+      "-" +
+      fechaActual.getDate().toString().padStart(2, "0");
+    this.fechaActual2 =
+      fechaActual.getFullYear() +
+      "-" +
+      (fechaActual.getMonth() + 1).toString().padStart(2, "0") +
+      "-" +
+      fechaActual.getDate().toString().padStart(2, "0") +
+      "T" +
+      fechaActual.getHours().toString().padStart(2, "0") +
+      ":" +
+      fechaActual.getMinutes().toString().padStart(2, "0") +
+      ":" +
+      fechaActual.getSeconds().toString().padStart(2, "0");
     this.formAccionCaja = this.formBuilder.group({
-      id: '',
-      cajaId:[this.datosCaja['id'],[ Validators.required]],
-      montoApertura:['', [Validators.required]],
-      fechaApertura:[this.fechaActual2,Validators.required],
-      descripcionApertura:['', [Validators.required,Validators.minLength(8),Validators.maxLength(255)]],
-      transacciones: this.formBuilder.array([])
+      id: "",
+      cajaId: [this.datosCaja["id"], [Validators.required]],
+      montoApertura: ["", [Validators.required]],
+      fechaApertura: [this.fechaActual2, Validators.required],
+      descripcionApertura: [
+        "",
+        [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.maxLength(255),
+        ],
+      ],
+      transacciones: this.formBuilder.array([]),
     });
   }
-  get form() {return this.formAccionCaja.controls}
-
-
-  recibirMontoTotal(monto){
-   this.totalApertura = monto;
-   this.formAccionCaja.get('montoApertura').setValue(monto);
+  get form() {
+    return this.formAccionCaja.controls;
   }
 
-  guardarForm(){
-    if(this.formAccionCaja.valid){
-      this.formAccionCaja.value['movimientoCajas']= this.formAccionCaja.value['transacciones'];      
-      this.movimientoCajaService.movimientoApertura(this.formAccionCaja.value).subscribe(data=>{
-        this.alActualizar.emit(data);
-        this.notificacionService.successStandar();
-      }, error=>this.notificacionService.alertError(error));
+  recibirMontoTotal(monto) {
+    this.totalApertura = monto;
+    this.formAccionCaja.get("montoApertura").setValue(monto);
+  }
+
+  guardarForm() {
+    if (this.formAccionCaja.valid) {
+      this.formAccionCaja.value["movimientoCajas"] =
+        this.formAccionCaja.value["transacciones"];
+      this.movimientoCajaService
+        .movimientoApertura(this.formAccionCaja.value)
+        .subscribe(
+          (data) => {
+            this.alActualizar.emit(data);
+            this.isStatusSubmit = false;
+            this.notificacionService.successStandar();
+          },
+          (error) => this.notificacionService.alertError(error)
+        );
     }
     this.submitted = true;
-   }
+  }
 
-   confirmAndContinueSaving = async (): Promise<void> => {
-		this.submitted = true
-
-		if (!this.formAccionCaja.valid) {
-			return
-		}
-		const dataImg = await this.screenshotService?.takeScreenshot('accountFormModalBodyDiv');
-		this.notificacionService?.confirmAndContinueAlert(dataImg, response =>{
-			if(response) this.guardarForm();
+  confirmAndContinueSaving = async (): Promise<void> => {
+    this.submitted = true;
+    this.isStatusSubmit = true;
+    if (!this.formAccionCaja.valid) {
+      this.isStatusSubmit = false;
+      return;
     }
-		)
-	}
-
+    const dataImg = await this.screenshotService?.takeScreenshot(
+      "accountFormModalBodyDiv"
+    );
+    this.notificacionService?.confirmAndContinueAlert(dataImg, (response) => {
+      if (response) this.guardarForm();
+      this.isStatusSubmit = false;
+    });
+  };
 }
