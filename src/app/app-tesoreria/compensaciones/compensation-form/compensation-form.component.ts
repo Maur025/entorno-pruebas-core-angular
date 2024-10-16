5eca34c6c2850f75470e4062e83113c4bc6c117b
Arnaldo Muñoz <kings31lendy@gmail.com>
2024-10-16 14:29:52 -0400

    KERTESO-549 Modificando el orderBy de cliente-credito

--- src/app/app-tesoreria/compensaciones/compensation-form/compensation-form.component.ts
+++ src/app/app-tesoreria/compensaciones/compensation-form/compensation-form.component.ts
@@ -205,7 +205,14 @@ export class CompensationFormComponent implements OnInit {
   getSClientCredit = (clientId: string, origin: boolean = true): void => {
     this.isEmptyOrigin = false;
     this.cobroService
-      ?.getSalesPendingByClientId(clientId, 0, 100, "razonSocial", false, true)
+      ?.getSalesPendingByClientId(
+        clientId,
+        0,
+        1000,
+        "nroFacturaRecibo",
+        true,
+        true
+      )
       ?.subscribe({
         next: (response: ApiResponseStandard) => {
           if (origin) {
